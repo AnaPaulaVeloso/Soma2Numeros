1a757e5219fb05ad0d147759f757ccd5012df129
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-09-17 19:09:06 -0300

    Concluir a tarefa Soma2Numero

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1,n2;
+            string NumeroDigitado;
+
+            Console.Write("Digite o 1 valor: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o 2 valor: "); 
+            NumeroDigitado = Console.ReadLine();
+            n2 = Convert.ToDouble(NumeroDigitado);
+
+            double resultado = n1 + n2;
+
+            Console.WriteLine($"Soma: {resultado}");   
+    }
+    }
+}   
